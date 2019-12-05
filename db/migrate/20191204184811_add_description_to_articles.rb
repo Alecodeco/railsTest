d61de6fcbdccbe0a7aa179bc32f99e528d3188f0
Alejandro Codeseira <acodeseira@ATM.Local>
2019-12-05 10:23:28 -0300

    DB fix

--- db/migrate/20191204184811_add_description_to_articles.rb
+++
@@ -1,7 +0,0 @@
-class AddDescriptionToArticles < ActiveRecord::Migration[5.2]
-  def change
-    add_column :articles, :description, :text
-    add_column :created_at, :updated_at, :datetime
-    add_column :articles, :updated_at, :datetime
-  end
-end
