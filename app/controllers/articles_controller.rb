fbf54915dd4b5f114c4bee9fef4336be02bb5f90
Alejandro Codeseira <acodeseira@ATM.Local>
2019-12-09 13:50:27 -0300

    List articles and links to go back to it.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,24 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Updated OK"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @article = Article.all 
+  end
+
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
