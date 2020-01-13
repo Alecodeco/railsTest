a720cdc8b676ddd165c9a00676541f190aba7356
Alejandro Codeseira <acodeseira@ATM.Local>
2020-01-13 14:22:23 -0300

    Complete authentication system. -Navbar actions and buttons. -Improved responsive views.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
 
 before_action :set_article, only: [:edit, :update, :show, :destroy]
+before_action :require_user, except: [:index, :show]
+before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def new
     @article = Article.new
@@ -28,7 +30,7 @@ before_action :set_article, only: [:edit, :update, :show, :destroy]
   def create
     #debugger
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article created OK"
       redirect_to article_path(@article)
