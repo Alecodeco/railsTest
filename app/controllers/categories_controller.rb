22bec500e7d1ac29239e2167804bd13f3534d82b
Alejandro Codeseira <acodeseira@ATM.Local>
2020-01-20 14:46:58 -0300

    New category action and test. Minor navbar fixes.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
 
+  before_action :require_admin, except: [:index, :show]
+
   def index
     @category = Category.paginate(page:params[:page], per_page: 8)
   end
@@ -25,5 +27,13 @@ class CategoriesController < ApplicationController
   def category_params
     params.require(:category).permit(:name)
   end
+
+  def require_admin
+    if !logged_in? || (logged_in? && !current_user.admin?)
+      flash[:danger] = "You need to be logged in as Admin for that."
+      redirect_to categories_path
+    end
+  end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_same_user, only: [:destroy, :edit, :update]
   before_action :require_admin, only: [:destroy]
 
   def set_user
@@ -56,7 +56,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if !logged_in? && current_user!= @user
+    if logged_in? && current_user != @user
       flash[:danger] = "You can only edit your own account"
       redirect_to root_path
     end
